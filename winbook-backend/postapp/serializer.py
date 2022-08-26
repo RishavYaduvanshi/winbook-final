4a67b7ccb49dcd5b0a3edcb8dc8d79a79d29d168
RishavYaduvanshi <rishavkhiladi768@gmail.com>
2022-08-26 15:34:17 +0530

    likes fix

--- winbook-backend/postapp/serializer.py
+++ winbook-backend/postapp/serializer.py
@@ -7,12 +7,21 @@ class PostSerializer(ModelSerializer):
     
     def get_userName(self, obj):
         return str(obj.user.username)
+
+
+
+
     userName = SerializerMethodField()
+    liked_cnt = SerializerMethodField()
+
+    def get_liked_cnt(self, obj):
+        return obj.liked_by.count()
+    
     
     class Meta:
         model = Post
         
-        fields = ('url', 'caption', 'liked_by', 'created_at', 'updated_at','pk', 'userName','user')
+        fields = ('url', 'caption', 'liked_cnt', 'created_at', 'updated_at','pk', 'userName','user')
         read_only_fields = ('created_at', 'updated_at')
         extra_kwargs = {
             'url': {'required': True},
