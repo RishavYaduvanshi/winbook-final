8d929dfe973c45cadf177d7b97a14f7cddfff592
Shacklebolt13 <19cse223.gagandeepsinghbhambrah@giet.edu>
2022-09-05 16:14:48 +0530

    added forgot password

---
+++ winbook-backend/authn/helpers/forgot_password.py
@@ -0,0 +1,26 @@
+import hashlib
+from rest_framework.authtoken.models import Token
+from django.template.loader import render_to_string
+from django.conf import settings
+
+
+def gen_forgot_mail(request, user):
+    """Generates the forgot password email"""
+    return render_to_string(
+        "authn/forgot-pass.html",
+        context={
+            "RESET_END": "forgot/?token=" + hash_token(get_token(user)),
+            "SITE_URL": settings.SITE_URL,
+        },
+    )
+
+
+def hash_token(token):
+    """Hashes the token"""
+    return hashlib.sha256(token.encode()).hexdigest()
+
+
+def get_token(user):
+    """Refreshes the token"""
+    token, _ = Token.objects.get_or_create(user=user)
+    return token.key
--- winbook-backend/socialBackend/settings.py
+++ winbook-backend/socialBackend/settings.py
@@ -16,79 +16,82 @@ from telnetlib import AUTHENTICATION
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_DIRS = [BASE_DIR / 'statics']
+STATICFILES_DIRS = [BASE_DIR / "statics"]
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t8a!epqd!s31a5ahz4^de!jsb1c6zwe9+v_yh*!jtp+1x+8y-r'
+SECRET_KEY = "django-insecure-t8a!epqd!s31a5ahz4^de!jsb1c6zwe9+v_yh*!jtp+1x+8y-r"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["winbookbackend.d3m0n1k.engineer","*"]
+ALLOWED_HOSTS = ["winbookbackend.d3m0n1k.engineer", "*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'corsheaders',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'authn',
-    'postapp',
-    'rest_framework',
-    'rest_framework.authtoken'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "corsheaders",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "authn",
+    "postapp",
+    "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     # 'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'socialBackend.urls'
+ROOT_URLCONF = "socialBackend.urls"
 
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['https://winbookbackend.d3m0n1k.engineer', 'http://localhost:3000']
+CSRF_TRUSTED_ORIGINS = [
+    "https://winbookbackend.d3m0n1k.engineer",
+    "http://localhost:3000",
+]
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'socialBackend.wsgi.application'
+WSGI_APPLICATION = "socialBackend.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -115,9 +118,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -127,35 +130,42 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # user Defined
 
-AUTH_USER_MODEL = 'authn.User'
-LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
+AUTH_USER_MODEL = "authn.User"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
+    "DEFAULT_PERMISSION_CLASSES": [],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
-    
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ]
 }
 
-STATIC_ROOT = BASE_DIR / 'static/'
-MEDIA_ROOT = BASE_DIR / 'media/'
-STATIC_URL = '/static/'
-MEDIA_URL =  '/media/'
+STATIC_ROOT = BASE_DIR / "static/"
+MEDIA_ROOT = BASE_DIR / "media/"
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 
-DATA_UPLOAD_MAX_MEMORY_SIZE = 10*1024*1024  # your size limit in bytes
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024  # your size limit in bytes
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "teamd3mon1k@gmail.com"
+EMAIL_HOST_PASSWORD = "qjvuohcoufldkpzq"
+EMAIL_USE_TLS = True
+
+SITE_URL = "https://winbook.d3m0n1k.engineer/"
