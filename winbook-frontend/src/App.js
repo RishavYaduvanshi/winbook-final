14c004b0f08960dab4dc4d85b3ba5ec993387088
Hasib00786 <hasibnirban007@gmail.com>
2022-09-14 15:58:11 +0530

    forgot password done

--- winbook-frontend/src/App.js
+++ winbook-frontend/src/App.js
@@ -4,9 +4,8 @@ import { Content } from "./componrnts/Content";
 import { Profilr } from "./componrnts/Profile/Profilr";
 import Forgot from "./componrnts/Forgot";
 import NotFound from "./componrnts/NotFound";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material";
-import { useSearchParams } from 'react-router-dom';
 import {
   Routes,
   Route,
@@ -17,19 +16,12 @@ function App() {
 
   var auth,email;
   const [mode, setMode] = useState(localStorage.getItem("theme"));
-  const [searchParams, setSearchParams] = useSearchParams();
   const darkTheme = createTheme({
     palette: {
       mode: mode,
     },
   });
 
-  useEffect(() => {
-    auth = searchParams.get("token");
-    email = searchParams.get("email");
-  }, [searchParams]);
-
-  console.log(auth,email);
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -37,7 +29,7 @@ function App() {
       <Route path="/profile" element={<Profilr setMode={setMode} mode={mode} />} />
       <Route path="*" element={<NotFound />} />
       <Route path="/" element={<Login setMode={setMode} mode={mode}/>} />
-      <Route path="/forgot" element={<Forgot setMode={setMode} mode={mode} auth={auth} eml={email}/>} />
+      <Route path="/forgot" element={<Forgot setMode={setMode} mode={mode}/>} />
       <Route path="/signup" element={<SignUp setMode={setMode} mode={mode}/>} />
       <Route path="/home" element={<Content setMode={setMode} mode={mode}/>} />
     </Routes>
--- winbook-frontend/src/componrnts/Forgot.jsx
+++ winbook-frontend/src/componrnts/Forgot.jsx
@@ -1,264 +1,267 @@
-import { Typography, Box, TextField, InputAdornment, Button, Card} from '@mui/material'
+import { Typography, Box, TextField, InputAdornment, Button, Card } from '@mui/material'
 import MailIcon from '@mui/icons-material/Mail';
 import KeyIcon from '@mui/icons-material/Key';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { useState } from 'react';
+import { useEffect,useState } from 'react';
 import Navbar from './Navbar';
-const Forgot = ({mode,setMode,auth,eml}) => {
-  console.log(eml);
-  console.log(auth);
-    const [passwordType, setPasswordType] = useState("password");
-    const [passwordInput, setPasswordInput] = useState("");
-    const [passwordInput1, setPasswordInput1] = useState("");
-    const [emailInput, setEmailInput] = useState("");
-    const handlePasswordChange =(evnt)=>{
-        setPasswordInput(evnt.target.value);
-    }
-    const handlePasswordChange_ =(evnt)=>{
-        setPasswordInput1(evnt.target.value);
-    }
-    const emailchange =(evnt)=>{
-        setEmailInput(evnt.target.value);
-    }
-    const togglePassword =()=>{
-        if(passwordType==="password")
-        {
-         setPasswordType("text")
-         return;
-        }
-        setPasswordType("password")
-      }
+import { useSearchParams } from 'react-router-dom';
 
-      const resetpass = () => {
-        if(passwordInput!==passwordInput1)
-        {
-          alert("Passwords do not match");
-          return;
-        }
-        fetch("https://winbookbackend.d3m0n1k.engineer/forgot/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Token " + localStorage.getItem('authtoken')
-          },
-          body: JSON.stringify({
-            password: passwordInput,
-            token: auth,
-          }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.error) {
-              alert(data.error);
-            } else {
-              alert("Password reset successful");
-            }
-          });
-      }
+const Forgot = ({ mode, setMode }) => {
+  document.title = "Winbook | Forgot Password";
+  var auth,eml,a,b;
+  const [passwordType, setPasswordType] = useState("password");
+  const [passwordInput, setPasswordInput] = useState("");
+  const [passwordInput1, setPasswordInput1] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
 
-      const resetp = (event)=> {
-        if(auth===null || eml===null){
-          eml = emailInput;
-          auth = null;
-        }
-        
-        //console.log(eml);
-        
 
-        fetch("https://winbookbackend.d3m0n1k.engineer/forgot/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: {
-            email: eml,
-          },
-        }).then((res) => res.json())
-          .then((data) => {
-            if (data.error) {
-              alert(data.error);
-            }
-            else{
-              alert("Confirmation email sent");
-            }
-          });
-      }
+  if (auth !== 'undefined' || eml !== 'undefined') {
+    eml = emailInput;
+    auth = null;
+  }else{
+    eml = searchParams.get('email');
+    auth = searchParams.get('token');
+  }
+  const handlePasswordChange = (evnt) => {
+    setPasswordInput(evnt.target.value);
+  }
+  const handlePasswordChange_ = (evnt) => {
+    setPasswordInput1(evnt.target.value);
+  }
+  const emailchange = (evnt) => {
+    setEmailInput(evnt.target.value);
+  }
+  const togglePassword = () => {
+    if (passwordType === "password") {
+      setPasswordType("text")
+      return;
+    }
+    setPasswordType("password")
+  }
+  const resetpass = () => {
+    if (passwordInput !== passwordInput1) {
+      alert("Passwords do not match");
+      return;
+    }
+    fetch("https://winbookbackend.d3m0n1k.engineer/forgot/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Token " + localStorage.getItem('authtoken')
+      },
+      body: JSON.stringify({
+        email: searchParams.get('email'),
+        password: passwordInput,
+        token: searchParams.get('token'),
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          alert(data.error);
+        } else {
+          alert("Password reset successful");
+          window.location.href = "/";
+        }
+      });
+  }
+  const resetp = (event) => {
+    var fd = {};
+    fd.email = eml;
+    console.log(JSON.stringify(fd));
+    fetch("https://winbookbackend.d3m0n1k.engineer/forgot/", {
+      method: "POST",
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+      body:JSON.stringify(fd),
+    }).then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          alert(data.error);
+        }
+        else {
+          alert("Confirmation email sent");
+        }
+      });
+  }
 
   return (
     <>
-    <Navbar setMode={setMode} mode={mode}/>
-{/* Desktop Display */}
-    <Box
-      sx={{
-        display: {sm:"flex",xs:"none"},
-        '& > :not(style)': {
-          m: 1,
-          height: '60vh',
-          width: '50%',
-          marginTop: '6%',
-          borderRadius: '10px',
-        },
-      }}
-      justifyContent="center"
-      justifyItems="center"
+      <Navbar setMode={setMode} mode={mode} />
+      {/* Desktop Display */}
+      <Box
+        sx={{
+          display: { sm: "flex", xs: "none" },
+          '& > :not(style)': {
+            m: 1,
+            height: '60vh',
+            width: '50%',
+            marginTop: '6%',
+            borderRadius: '10px',
+          },
+        }}
+        justifyContent="center"
+        justifyItems="center"
       >
-      <Card elevation={12} sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }} >
-        <Typography variant='h5' color="primary" sx={{marginTop:'5%'}}>Forgot Password</Typography>
-        <Box sx={{
+        <Card elevation={12} sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }} >
+          <Typography variant='h5' color="primary" sx={{ marginTop: '5%' }}>Forgot Password</Typography>
+          <Box sx={{
             display: 'flex',
             flexDirection: 'column',
-            marginTop:'10%',
+            marginTop: '10%',
             width: '70%',
 
-        }}>
-      {typeof auth!=='undefined'?<>
-        <TextField type={passwordType} id="password" name="password" onChange={handlePasswordChange_} label="New Password" variant="outlined" required fullWidth
-       InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <KeyIcon />
-          </InputAdornment>
-        ),
-        endAdornment: (
-            <InputAdornment position="end">
-               {passwordType==="password"?<VisibilityIcon onClick={togglePassword}/>:<VisibilityOffIcon onClick={togglePassword}/>}
-                </InputAdornment>
-        )
-      }}
-      sx={{marginBottom: 2}}
-      />
-      <TextField type={passwordType} id="cnfpassword" onChange={handlePasswordChange} name="cnfpassword" label="Confirm Password Password" variant="outlined" required fullWidth
-       InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <KeyIcon />
-          </InputAdornment>
-        ),
-        endAdornment: (
-            <InputAdornment position="end">
-               {passwordType==="password"?<VisibilityIcon onClick={togglePassword}/>:<VisibilityOffIcon onClick={togglePassword}/>}
-                </InputAdornment>
-        )
-      }}
-      sx={{marginBottom: 5}}
-      />
-      <Button variant="contained" color="primary" sx={{marginBottom:7}} onClick={resetpass}>
-        Change Password
-      </Button>
-      </>:<>
-      <TextField id="email" name="email" onChange={emailchange} label="Enter your Email" variant="outlined" required fullWidth
-       InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <MailIcon />
-          </InputAdornment>
-        ),
-      }}
-      />
-      <Typography sx={{
-        marginBottom: 10,
-        fontWeight:300,
-      }}>Please provide your registered Email ID !</Typography>
-      <Button variant="contained" color="primary" sx={{
-        marginBottom: 7,
-      }} onClick={resetp}>
-        Submit
-      </Button></>}
-      </Box>
-      </Card>
+          }}>
+            {typeof searchParams.get("token") !== 'undefined' && searchParams.get("token") !== null ? <>
+              <TextField type={passwordType} id="password" name="password" onChange={handlePasswordChange_} label="New Password" variant="outlined" required fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <KeyIcon />
+                    </InputAdornment>
+                  ),
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      {passwordType === "password" ? <VisibilityIcon onClick={togglePassword} /> : <VisibilityOffIcon onClick={togglePassword} />}
+                    </InputAdornment>
+                  )
+                }}
+                sx={{ marginBottom: 2 }}
+              />
+              <TextField type={passwordType} id="cnfpassword" onChange={handlePasswordChange} name="cnfpassword" label="Confirm Password Password" variant="outlined" required fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <KeyIcon />
+                    </InputAdornment>
+                  ),
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      {passwordType === "password" ? <VisibilityIcon onClick={togglePassword} /> : <VisibilityOffIcon onClick={togglePassword} />}
+                    </InputAdornment>
+                  )
+                }}
+                sx={{ marginBottom: 5 }}
+              />
+              <Button variant="contained" color="primary" sx={{ marginBottom: 7 }} onClick={resetpass}>
+                Change Password
+              </Button>
+            </> : <>
+              <TextField id="email" type="email" name="email" onChange={emailchange} label="Enter your Email" variant="outlined" required fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MailIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Typography sx={{
+                marginBottom: 10,
+                fontWeight: 300,
+              }}>Please provide your registered Email ID !</Typography>
+              <Button variant="contained" color="primary" sx={{
+                marginBottom: 7,
+              }} onClick={resetp}>
+                Submit
+              </Button></>}
+          </Box>
+        </Card>
       </Box>
 
-{/* Mobile Display */}
+      {/* Mobile Display */}
       <Box
-      sx={{
-        display: {sm:"none",xs:"block"},
-        '& > :not(style)': {
-          m: 1,
-          height: 'auto',
-          marginTop: '20%',
-          borderRadius: '10px',
-        },
-      }}
-      justifyContent="center"
+        sx={{
+          display: { sm: "none", xs: "block" },
+          '& > :not(style)': {
+            m: 1,
+            height: 'auto',
+            marginTop: '20%',
+            borderRadius: '10px',
+          },
+        }}
+        justifyContent="center"
       >
-      <Card elevation={12} sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }} >
-        <Typography variant='h5' color="primary" sx={{marginTop:'5%'}}>Forgot Password</Typography>
-        <Box sx={{
+        <Card elevation={12} sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }} >
+          <Typography variant='h5' color="primary" sx={{ marginTop: '5%' }}>Forgot Password</Typography>
+          <Box sx={{
             display: 'flex',
             flexDirection: 'column',
-            marginTop:'10%',
+            marginTop: '10%',
             width: '80%',
 
-        }}>
-      {typeof auth!=='undefined'?<>
-        <TextField type={passwordType} id="password" name="password" onChange={handlePasswordChange_} label="New Password" variant="outlined" required fullWidth
-       InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <KeyIcon />
-          </InputAdornment>
-        ),
-        endAdornment: (
-            <InputAdornment position="end">
-               {passwordType==="password"?<VisibilityIcon onClick={togglePassword}/>:<VisibilityOffIcon onClick={togglePassword}/>}
-                </InputAdornment>
-        )
-      }}
-      sx={{marginBottom: 2}}
-      />
-      <TextField type={passwordType} id="cnfpassword" onChange={handlePasswordChange} name="cnfpassword" label="Confirm Password Password" variant="outlined" required fullWidth
-       InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <KeyIcon />
-          </InputAdornment>
-        ),
-        endAdornment: (
-            <InputAdornment position="end">
-               {passwordType==="password"?<VisibilityIcon onClick={togglePassword}/>:<VisibilityOffIcon onClick={togglePassword}/>}
-                </InputAdornment>
-        )
-      }}
-      sx={{marginBottom: 5}}
-      />
-      <Button variant="contained" color="primary" sx={{marginBottom:7}} onClick={resetpass}>
-        Change Password
-      </Button>
-      </>:<>
-      <TextField id="email" name="email" onChange={emailchange} label="Enter your Email" variant="outlined" required fullWidth
-       InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <MailIcon />
-          </InputAdornment>
-        ),
-      }}
-      />
-      <Typography sx={{
-        marginBottom: 10,
-        fontWeight:300,
-      }}>Please provide your registered Email ID !</Typography>
-      <Button variant="contained" color="primary" sx={{
-        marginBottom: 7,
-      }} onClick={resetp}>
-        Submit
-      </Button></>}
+          }}>
+            {typeof searchParams.get("token") !== 'undefined' && searchParams.get("token") !== null ? <>
+              <TextField type={passwordType} id="password" name="password" onChange={handlePasswordChange_} label="New Password" variant="outlined" required fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <KeyIcon />
+                    </InputAdornment>
+                  ),
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      {passwordType === "password" ? <VisibilityIcon onClick={togglePassword} /> : <VisibilityOffIcon onClick={togglePassword} />}
+                    </InputAdornment>
+                  )
+                }}
+                sx={{ marginBottom: 2 }}
+              />
+              <TextField type={passwordType} id="cnfpassword" onChange={handlePasswordChange} name="cnfpassword" label="Confirm Password Password" variant="outlined" required fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <KeyIcon />
+                    </InputAdornment>
+                  ),
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      {passwordType === "password" ? <VisibilityIcon onClick={togglePassword} /> : <VisibilityOffIcon onClick={togglePassword} />}
+                    </InputAdornment>
+                  )
+                }}
+                sx={{ marginBottom: 5 }}
+              />
+              <Button variant="contained" color="primary" sx={{ marginBottom: 7 }} onClick={resetpass}>
+                Change Password
+              </Button>
+            </> : <>
+              <TextField id="email" name="email" type="email" onChange={emailchange} label="Enter your Email" variant="outlined" required fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MailIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Typography sx={{
+                marginBottom: 10,
+                fontWeight: 300,
+              }}>Please provide your registered Email ID !</Typography>
+              <Button variant="contained" color="primary" sx={{
+                marginBottom: 7,
+              }} onClick={resetp}>
+                Submit
+              </Button></>}
+          </Box>
+        </Card>
       </Box>
-      </Card>
-      </Box>
-      </>
-      
+    </>
+
   )
 }
 
