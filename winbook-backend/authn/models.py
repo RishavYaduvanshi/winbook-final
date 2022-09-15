0aca290157acd35fd847405821d81c8335944c91
Shacklebolt13 <19cse223.gagandeepsinghbhambrah@giet.edu>
2022-09-15 10:42:29 +0530

    feat: :sparkles: added default values to dp and cover

--- winbook-backend/authn/models.py
+++ winbook-backend/authn/models.py
@@ -4,8 +4,11 @@ from django.contrib.auth.models import AbstractUser as _User
 
 # Create your models here.
 
+
 class User(_User):
-    
-    bio = models.TextField(blank=True,default="Add Bio",null=True)
-    dp = models.ImageField(upload_to='dp/',null=True,blank=True)
-    cover = models.ImageField(upload_to='covers/',null=True,blank=True)
+
+    bio = models.TextField(blank=True, default="Add Bio", null=True)
+    dp = models.ImageField(upload_to="dp/", blank=True, default="/static/authn/dp.png")
+    cover = models.ImageField(
+        upload_to="covers/", blank=True, default="/static/authn/cover.png"
+    )
